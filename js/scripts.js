f7bcb7fd8b2b4fd6365e674f89f36e12a765f013
daynearron <dwatai@commercemtg.com>
2017-02-23 14:09:57 -0800

    page transition

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
     $(".project-thumb-container li").on("mouseover",function(){
         $(".project-thumb-container").addClass($(this).data('ul-class')); 
         $(".transition").addClass('transition-white'); 
@@ -7,5 +7,21 @@ $(document).ready(function(){
         $(".project-thumb-container").removeClass($(this).data('ul-class'));  
         $(".transition").removeClass('transition-white'); 
     });
+	//Animsition
+	$(".animsition").animsition({
+		inClass: 'fade-in-down-sm',
+		outClass: 'fade-out-left-sm',
+		inDuration: 500,
+		outDuration: 500,
+		linkElement: '.animsition-link',
+		// e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+		loading: true,
+		loadingParentElement: 'body', //animsition wrapper element
+		loadingClass: 'animsition-loading',
+		unSupportCss: [
+		  'animation-duration',
+		  '-webkit-animation-duration',
+		  '-o-animation-duration'
+		]
+	});
 });
-
