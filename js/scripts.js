a2cc893c6cb65b3589c00ec21c7e0a068674358f
daynearron <dwatai@commercemtg.com>
2017-06-30 12:43:14 -0700

    new stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(function(){
 
-    $(".project-thumb-container li").on("mouseover",function(){
-        $(".project-thumb-container").addClass($(this).data('ul-class')); 
-        $(".transition").addClass('transition-white'); 
-    });
-    $(".project-thumb-container li").on("mouseout",function(){
-        $(".project-thumb-container").removeClass($(this).data('ul-class'));  
-        $(".transition").removeClass('transition-white'); 
-    });
+	$(".project-thumb-container li").on("mouseover",function(){
+		$(".project-thumb-container").addClass($(this).data('ul-class')); 
+		$(".transition").addClass('transition-white'); 
+	});
+	$(".project-thumb-container li").on("mouseout",function(){
+		$(".project-thumb-container").removeClass($(this).data('ul-class'));  
+		$(".transition").removeClass('transition-white'); 
+	});
 	//Animsition
 	$(".animsition").animsition({
 		inClass: 'fade-in-up-sm',
@@ -25,8 +25,9 @@ $(function(){
 		  '-o-animation-duration'
 		]
 	});
-
 });
+
+
 
 
 
